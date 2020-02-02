89cbb3f5806c2efa2f05d88a6de99f707e7fa5a7
suewong <951828649@qq.com>
2020-02-02 13:53:27 +0800

    谷子表格样式修改

--- src/components/pages/GoodsSearch.vue
+++ src/components/pages/GoodsSearch.vue
@@ -1,84 +1,112 @@
 <template>
   <div>
 
-    <!--<el-container>
+    <el-container>
       <el-header>谷子查询</el-header>
       <el-container>
         <el-main>
+          <el-table
+            :data="tableData"
+            style="width: 100%"
+            :header-row-style="rowClass">
+            <el-table-column
+              prop="date"
+              label="排单日期">
+            </el-table-column>
+            <el-table-column
+              prop="name"
+              label="谷子名称">
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              label="谷子类型">
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              label="角色">
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              label="谷子图片">
+            </el-table-column>
+            <!--<el-table-column
+              prop="address"
+              label="动漫名称">
+            </el-table-column>-->
+            <!--<el-table-column
+              prop="address"
+              label="来源">
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              label="群号/闲鱼名称">
+            </el-table-column>-->
+            <el-table-column
+              prop="address"
+              label="是否全款">
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              label="定金">
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              label="全款">
+            </el-table-column>
+            <!--<el-table-column
+              prop="date"
+              label="出谷日期">
+            </el-table-column>-->
+            <el-table-column
+              prop="address"
+              label="是否包邮">
+            </el-table-column>
+            <!--<el-table-column
+              prop="address"
+              label="邮费">
+            </el-table-column>-->
+            <el-table-column
+              prop="address"
+              label="是否到货">
+            </el-table-column>
+            <!--<el-table-column
+              prop="address"
+              label="总价">
+            </el-table-column>-->
 
+            <el-table-column type="expand">
+             <template slot-scope="props">
+                <el-form label-position="left" inline class="demo-table-expand">
+                  <el-form-item label="动漫名称">
+                    <span>{{ props.row.name }}</span>
+                  </el-form-item>
+
+                  <el-form-item label="出谷日期">
+                    <span>{{ props.row.name }}</span>
+                  </el-form-item>
+                  <el-form-item label="来源">
+                    <span>{{ props.row.name }}</span>
+                  </el-form-item>
+                  <el-form-item label="群号/闲鱼号/微博号">
+                    <span>{{ props.row.name }}</span>
+                  </el-form-item>
+                  <el-form-item label="邮费">
+                    <span>{{ props.row.name }}</span>
+                  </el-form-item>
+
+                  <el-form-item label="总价">
+                    <span>{{ props.row.name }}</span>
+                  </el-form-item>
+                </el-form>
+              </template>
+
+            </el-table-column>
+          </el-table>
         </el-main>
       </el-container>
-    </el-container>-->
+    </el-container>
 
-    <el-table
-      :data="tableData"
-      style="width: 100%"
-      :header-row-style="rowClass">
-      <el-table-column
-        prop="date"
-        label="排单日期">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="谷子名称">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="谷子类型">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="角色">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="谷子图片">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="动漫名称">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="来源">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="群号/闲鱼名称">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="是否全款">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="定金">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="全款">
-      </el-table-column>
-      <el-table-column
-        prop="date"
-        label="出谷日期">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="是否包邮">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="邮费">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="是否到货">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="总价">
-      </el-table-column>
-    </el-table>
+
   </div>
 
 
@@ -88,7 +116,7 @@
 <script>
 
   export default {
-    data() {
+    data () {
       return {
         tableData: [{
           date: '2016-05-02',
@@ -112,7 +140,7 @@
 
     methods: {
       // 设置表格第一行的颜色
-      rowClass({ row, rowIndex}) {
+      rowClass ({row, rowIndex}) {
         console.log(rowIndex) //表头行下标
         return 'background:#F3F4F7;color:#555'
       }
@@ -123,10 +151,24 @@
 
 <style>
 
-  .el-table__header{
+  .el-table__header {
     height: 35px;
     line-height: 50px;
   }
 
 
+  .demo-table-expand {
+    font-size: 0;
+    line-height: 15px;
+  }
+  .demo-table-expand label {
+    width: auto;
+    color: #99a9bf;
+  }
+
+  .demo-table-expand .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+    width: 50%;
+  }
 </style>
